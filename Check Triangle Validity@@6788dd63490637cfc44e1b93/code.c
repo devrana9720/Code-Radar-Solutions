aa9b0f7c0d071e095828c73cacb6c8a736605c34
devrana9720 <devrana3256@gmail.com>
2025-03-28 16:50:25 +0530

    Solution to Check Triangle Validity

--- Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
+++ Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <math.h>
 int main(){
-    int a,b;
+    float a,b;
     scanf("%d%d",&a,&b);
-    int c=((a*a)+(b*b));
-    int square=sqrt(c);
-    int d=sqrt(c);
+    float c=((a*a)+(b*b));
+    float square=sqrt(c);
+    float d=sqrt(c);
     if(d==c){
         printf("Valid");
     }
